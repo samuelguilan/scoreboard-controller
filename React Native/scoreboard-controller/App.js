906b0e01c00b1b7db7a74fb8e8ce9d5c2afb3111
samuelguilan <samuel.landre@hotmail.com>
2023-11-24 20:01:57 -0300

    Renaming alerts

--- React Native/scoreboard-controller/App.js
+++ React Native/scoreboard-controller/App.js
@@ -9,46 +9,46 @@ export default function App() {
       <View style={styles.buttonContainer}>
         <Pressable
           style={styles.buttonHomeStyle}
-          onPress={() => alert('You pressed a button.')}>
+          onPress={() => alert('Ponto para equipe da casa adicionado')}>
           <Text style={styles.buttonText}>Casa + 1</Text>
         </Pressable>
 
         <Pressable
           style={styles.buttonVisitorStyle}
-          onPress={() => alert('You pressed a button.')}>
+          onPress={() => alert('Ponto para equipe visitante adicionado')}>
           <Text style={styles.buttonText}>Visitante + 1</Text>
         </Pressable>
       </View>
       <View style={styles.buttonContainer}>
         <Pressable
           style={styles.buttonHomeStyle}
-          onPress={() => alert('You pressed a button.')}>
+          onPress={() => alert('Tempo da equipe da casa debitado')}>
           <Text style={styles.buttonText}>Tempo Casa</Text>
         </Pressable>
 
         <Pressable
           style={styles.buttonVisitorStyle}
-          onPress={() => alert('You pressed a button.')}>
+          onPress={() => alert('Tempo da equipe visitante debitado')}>
           <Text style={styles.buttonText}>Tempo Visitante</Text>
         </Pressable>
       </View>
       <View style={styles.buttonContainer}>
         <Pressable
           style={styles.buttonHomeStyle}
-          onPress={() => alert('You pressed a button.')}>
+          onPress={() => alert('Falta da equipe da casa adicionada')}>
           <Text style={styles.buttonText}>Falta Casa</Text>
         </Pressable>
 
         <Pressable
           style={styles.buttonVisitorStyle}
-          onPress={() => alert('You pressed a button.')}>
+          onPress={() => alert('Falta da equipe visitante adicionada')}>
           <Text style={styles.buttonText}>Falta Visitante</Text>
         </Pressable>
       </View>
       <Text style={styles.paragraph}>Relógio da partida</Text>
       <Pressable
         style={styles.buttonGameClockStyle}
-        onPress={() => alert('You pressed a button.')}>
+        onPress={() => alert('Relógio da partida iniciou/pausou')}>
         <MaterialCommunityIcons name="play-pause" size={100} color="black" />
       </Pressable>
 
@@ -56,14 +56,14 @@ export default function App() {
       <View style={styles.buttonContainer}>
         <Pressable
           style={styles.buttonShotClockStyle}
-          onPress={() => alert('You pressed a button.')}>
+          onPress={() => alert('Relógio de posse reiniciou para 14 segundos')}>
           <Text style={styles.buttonText}>14s</Text>
           <MaterialCommunityIcons name="restart" size={50} color="black" />
         </Pressable>
 
         <Pressable
           style={styles.buttonShotClockStyle}
-          onPress={() => alert('You pressed a button.')}>
+          onPress={() => alert('Relógio de posse reiniciou para 24 segundos')}>
           <Text style={styles.buttonText}>24s</Text>
           <MaterialCommunityIcons name="restart" size={50} color="black" />
         </Pressable>
@@ -72,7 +72,7 @@ export default function App() {
       <Text style = {styles.buttonTexts}>  </Text>
         <Pressable
           style={styles.buttonShotClockStyle}
-          onPress={() => alert('Pausou ')}>
+          onPress={() => alert('Relógio de posse resumiu')}>
           <MaterialCommunityIcons name="play" size={100} color="black" />
         </Pressable>
       
